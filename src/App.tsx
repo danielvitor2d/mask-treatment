13fd248d00ec3ed770b9054126c61e92c054bfdb
danielvitor2d <ripsea.rpg321@gmail.com>
2022-02-25 18:22:53 -0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from 'react';
+import Input from './components/Input';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Usuario {
+  cep: string
+  cpf: string
+  price: number
 }
 
-export default App;
+const App: React.FC = () => {
+  const [usuario, setUsuario] = useState<Usuario>({} as Usuario)
+
+  const handleChange = useCallback((e: React.FormEvent<HTMLInputElement>) => {
+    setUsuario({
+      ...usuario,
+      [e.currentTarget.name]: e.currentTarget.value
+    })
+  }, [usuario])
+
+  return (
+    <div>
+      <Input placeholder='99999-999' maxLength={8} name='cep' onKeyUp={handleChange} mask='cep' />
+      <Input placeholder='12.300,00' prefix='R$' name='price' onKeyUp={handleChange} mask='currency' />
+      <Input placeholder='987.490.720-72' maxLength={11} name='cpf' onKeyUp={handleChange} mask='cpf' />
+      <button onClick={() => console.log(usuario)} >Salvar</button>
+    </div>
+  )
+}
+
+export default App
